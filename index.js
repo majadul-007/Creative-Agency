f0c8f11398d559eae7ba368bcefecad9fcfd1d5c
tusarhaque <tusarhaque12@gmail.com>
2021-07-27 17:03:18 +0600

    first

--- index.js
+++ index.js
@@ -14,31 +14,21 @@ const app = express()
 
 app.use(express.json());
 app.use(cors());
-//app.use(express.static('doctors'));
+
 app.use(fileUpload());
 
-const port = 5000
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-// if (process.env.NODE_ENV === 'production') {
-//   // Exprees will serve up production assets
-//   app.use(express.static('client/build'));
-
-//   // Express serve up index.html file if it doesn't recognize route
-//   // const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-//
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productsCollection = client.db(" creative").collection(`AgencyServices`);
-  const OrderCollection = client.db(" creative").collection("AllOrder");
-  const reviewCollection = client.db(" creative").collection("Review");
+  const productsCollection = client.db("creative").collection("AgencyServices");
+  const OrderCollection = client.db("creative").collection("AllOrder");
+  const reviewCollection = client.db("creative").collection("Review");
   const adminCollection = client.db("creative").collection("Admin");
 
 
@@ -158,4 +148,4 @@ client.connect(err => {
 
 });
 
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || 5000)
